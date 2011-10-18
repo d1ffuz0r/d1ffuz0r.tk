28cd74f58e996cb13b34f076dbab7d934b8e654b
d1ffuz0r <d1fffuz0r@gmail.com>
2011-10-18 10:39:55 +0400

    disable in admin panel User and Group menus

--- homesite/admin.py
+++ homesite/admin.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
 from models import Services, Portfolio, Settings, About, QuickMessages, Blog
+from django.contrib.auth.models import User, Group
 
 class AdminServices(admin.ModelAdmin):
     pass
@@ -26,3 +27,5 @@ admin.site.register(Settings, AdminSettings)
 admin.site.register(About, AdminAbout)
 admin.site.register(QuickMessages, AdminQuickMessage)
 admin.site.register(Blog,AdminBlog)
+admin.site.unregister(User)
+admin.site.unregister(Group)
