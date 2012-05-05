f05894218084247a024b373ee312ade2a023533c
d1ffuz0r <d1fffuz0r@gmail.com>
2012-05-05 22:08:03 +0400

    disquis comments

--- homesite/views.py
+++ homesite/views.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.core.paginator import Paginator, InvalidPage, EmptyPage
 from django.shortcuts import get_object_or_404
 from models import Blog
--- settings.py
+++ settings.py
@@ -3,6 +3,7 @@ import os
 
 abs_path = lambda path: os.path.join(os.path.dirname(__file__), path)
 
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -14,12 +15,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'home',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'home',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
@@ -28,51 +29,34 @@ LANGUAGE_CODE = 'en-us'
 
 SITE_ID = 1
 
-#south
 SOUTH_TESTS_MIGRATE = False
 SKIP_SOUTH_TESTS=True
 
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
-USE_I18N = False
+USE_L10N = USE_I18N = False
 
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale
-USE_L10N = False
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
 MEDIA_ROOT = abs_path(os.path.join('public', 'site_media'))
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a trailing slash.
 MEDIA_URL = '/media/'
 
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
 #for DEBUG = False
 STATIC_ROOT = ''
 
 # URL prefix for static files.
 STATIC_URL = '/static/'
 
-# URL prefix for admin static files -- CSS, JavaScript and images.
-# Make sure to use a trailing slash.
 ADMIN_MEDIA_PREFIX = '/static/admin_media/'
 
-# Additional locations of static files
 STATICFILES_DIRS = (
     abs_path(os.path.join('public', 'static')),
 )
-# List of finder classes that know how to find static files in various locations.
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
-# Make this unique, and don't share it with anybody.
 SECRET_KEY = 'c6pbe#!2ej8+^^bik^=m6bg2i^_*96_vtc)w5en$7wd=&6%zj4'
 
-# List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
@@ -90,21 +74,25 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'src.urls'
 
-TEMPLATE_DIRS = abs_path(os.path.join('public', 'templates')) #devel
-#TEMPLATE_DIRS = ('./public/templates/',) #host
+TEMPLATE_DIRS = abs_path(os.path.join('public', 'templates'))
 
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    #'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
-    #'django.contrib.comments',
+    'django.contrib.sites',
     'src.homesite',
+    'disquis',
     'south',
-)# 'django.contrib.admindocs',
+)
+
+
+DISQUS_API_KEY = ''
+DISQUS_WEBSITE_SHORTNAME = 'd1ffuz0rhomesite'
+
 
 LOGGING = {
     'version': 1,
