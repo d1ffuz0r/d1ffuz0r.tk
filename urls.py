f6b1b2dd4068f90582c1b8bcaf4a60cc8c9aef29
d1ffuz0r <d1fffuz0r@gmail.com>
2012-09-27 23:33:06 +0400

    favicon, static and media through nginx

--- urls.py
+++ urls.py
@@ -9,13 +9,5 @@ handler500 = 'homesite.views.error500'
 
 urlpatterns = patterns('',
     url(r'', include('homesite.urls')),
-    url(r'^media/(?P<path>.*)$',
-        'django.views.static.serve',
-            {'document_root': settings.MEDIA_ROOT}
-    ),
-    url(r'^static/(?P<path>.*)$',
-        'django.views.static.serve',
-            {'document_root': settings.STATIC_ROOT}
-    ),
     url(r'^admin/', include(admin.site.urls)),
 )
